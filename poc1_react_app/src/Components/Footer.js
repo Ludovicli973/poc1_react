1e671bbcd8001d19f1b7241b32c20908f7fd10d5
Ludovic <94985180+Ludovicli973@users.noreply.github.com>
2023-02-10 14:42:20 +0100

    Update Footer.js

--- poc1_react_app/src/Components/Footer.js
+++ poc1_react_app/src/Components/Footer.js
@@ -1,13 +1,5 @@
 const Footer = () => {
-  return (
-    <div>
-      <h1>Zenika</h1>
-      <image
-        src="https://www.zenika.com/wp-content/uploads/2019/04/logo-zenika.png"
-        alt="Zenika Logo"
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Footer;
