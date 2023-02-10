a7f298b21bea9a457ad48089e3181ba4ef9828af
Ludovic <94985180+Ludovicli973@users.noreply.github.com>
2023-02-10 14:28:03 +0100

    adding the header

--- poc1_react_app/src/Components/Header.js
+++ poc1_react_app/src/Components/Header.js
@@ -1,5 +1,15 @@
+import Logo from "../assets/images/zenika_logo.png";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <div class="w-full h-16 flex items-center space-x-4">
+      <img src={Logo} alt="Zenika Logo" class="h-full aspect-auto" />
+      <a>Valeurs</a>
+      <a>Offres</a>
+      <a>Green IT</a>
+      <a>Sécurité</a>
+    </div>
+  );
 };
 
 export default Header;
