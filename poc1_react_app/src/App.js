989f2b88dab49bfd0f90e3e385117118bd0b0673
edouardglx <edouard.gloux@zenika.com>
2023-02-10 14:53:57 +0100

    Routes
    home et detail

--- poc1_react_app/src/App.js
+++ poc1_react_app/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 // impor
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Details from "./Pages/Details";
+import Home from "./Pages/Home";
 
 //COMPONENTS
 import Footer from "./Components/Footer";
@@ -10,8 +12,8 @@ function App() {
     <Router>
       <div class="w-full h-screen border-2 bg-orange-400 box-border">
         <Routes>
-          <Route />
-          <Route />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/details/:id" element={<Details />} />
         </Routes>
       </div>
       <Footer />
