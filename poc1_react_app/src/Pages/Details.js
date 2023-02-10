1c8bdb06015aced1ac9a05cf4f5b82fc95f2501c
Ludovic <94985180+Ludovicli973@users.noreply.github.com>
2023-02-10 17:41:05 +0100

    Update Details.js

--- poc1_react_app/src/Pages/Details.js
+++ poc1_react_app/src/Pages/Details.js
@@ -1,5 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Details = () => {
-  return <div></div>;
+  let { id } = useParams();
+  const [anime, setAnime] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const handleLoading = () => {
+    setLoading(false);
+  };
+
+  const handleAnime = (current_anime) => {
+    setAnime(current_anime);
+  };
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+        "X-RapidAPI-Host": "anime-db.p.rapidapi.com",
+      },
+    };
+
+    fetch(`https://anime-db.p.rapidapi.com/anime/by-id/${id}`, options)
+      .then((res) => res.json())
+      .then((data) => handleAnime(data));
+
+    handleLoading();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <h1>Chargement...</h1>
+      ) : (
+        <div class="w-full flex space-x-4 p-4">
+          <img src={anime.image} alt={anime.title} class="h-full aspect-auto" />
+          <div class="flex-col">
+            <h1 class="font-bold text-xl">{anime.title}</h1>
+            <p class="my-4 text-justify">{anime.synopsis}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Details;
