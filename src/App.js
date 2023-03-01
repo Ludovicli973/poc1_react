ec185eb3916c1a7ab54dd9ce05355365ca4b6388
IssamLynx <hm_bakha@esi.dz>
2023-03-01 14:45:41 +0100

    replaced /home by /

--- src/App.js
+++ src/App.js
@@ -18,9 +18,8 @@ function App() {
       <Header />
       <div class="w-full h-auto box-border">
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<Navigate to="/home" />} />
         </Routes>
       </div>
       <Footer />
