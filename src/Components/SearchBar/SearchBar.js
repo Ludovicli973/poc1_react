589ff25a28a52b5d0c494235d35a92933cb8d381
Ludovic <ludovicli973@gmail.com>
2023-03-31 10:55:17 +0200

    Modified variables name and changed the CSS of the buttons

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,66 +1,46 @@
 import { useState } from "react";
 
-function SearchBar({ initialData, setData }) {
-  const [animeName, setAnimeName] = useState("");
+function SearchBar({ initialAnimeList, setAnimeList }) {
+  const [searchInput, setsearchInput] = useState("");
 
   function handleSearch(e) {
     e.preventDefault();
-    const anime = initialData.filter((element) =>
-      element.title.includes(animeName)
+    const anime = initialAnimeList.filter((element) =>
+      element.title.includes(searchInput)
     );
-    setData(anime);
+    setAnimeList(anime);
   }
 
   function resetSearch() {
-    setData(initialData);
-    setAnimeName("");
+    setAnimeList(initialAnimeList);
+    setsearchInput("");
   }
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center mt-2 ml-4">
       <form onSubmit={handleSearch}>
-        <div className="flex space-x-1">
-          <input
-            onChange={(event) => {
-              setAnimeName(event.target.value);
-            }}
-            type="text"
-            className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-            placeholder="Search..."
-            value={animeName}
-          />
+        <input
+          type="text"
+          onChange={(e) => setsearchInput(e.target.value)}
+          name="input"
+          value={searchInput}
+          class="w-[200px] h-[30px] border border-black rounded"
+          placeholder="Search"
+        />
+        <button
+          class="ml-[10px] w-[100px] h-[30px] border border-black rounded bg-black text-white cursor-pointer"
+          type="submit"
+        >
+          Search
+        </button>
+        {searchInput !== "" && (
           <button
-            type="submit"
-            className="px-4 text-white bg-purple-600 rounded-full "
-            aria-label="search_button"
-            id="search_button"
+            class="ml-[10px] w-[100px] h-[30px] border rounded bg-gray-200 border-gray-200"
+            onClick={resetSearch}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+            Clear
           </button>
-          {animeName !== "" && (
-            <button
-              type="button"
-              onClick={resetSearch}
-              id="clear_button"
-              className="px-4 text-purple-600 border border-purple-600 rounded-full "
-            >
-              Clear
-            </button>
-          )}
-        </div>
+        )}
       </form>
     </div>
   );
