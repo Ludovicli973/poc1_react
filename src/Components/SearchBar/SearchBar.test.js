022ff8308df54653a176ba8c70530751c0ef1535
Ludovic <ludovicli973@gmail.com>
2023-04-11 14:51:48 +0200

    Corrected the searchbar.test.js file

--- src/Components/SearchBar/SearchBar.test.js
+++ src/Components/SearchBar/SearchBar.test.js
@@ -2,12 +2,17 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SearchBar from "./SearchBar";
 
-const mockedSetData = jest.fn();
-const initialData = [];
+const mockedSetAnimeList = jest.fn();
+const initialAnimeList = [];
 
 describe("When the searchbar is empty", () => {
   test("The clear button should be invisible", async () => {
-    render(<SearchBar initialData={initialData} setData={mockedSetData} />);
+    render(
+      <SearchBar
+        initialAnimeList={initialAnimeList}
+        setData={mockedSetAnimeList}
+      />
+    );
 
     const clear_button = await screen.queryByRole("button", {
       name: "Clear",
@@ -18,9 +23,14 @@ describe("When the searchbar is empty", () => {
 
 describe("When the searchbar is not empty", () => {
   test("The clear button should be visible", async () => {
-    render(<SearchBar initialData={initialData} setData={mockedSetData} />);
+    render(
+      <SearchBar
+        initialAnimeList={initialAnimeList}
+        setData={mockedSetAnimeList}
+      />
+    );
 
-    const input = await screen.findByPlaceholderText("Search...");
+    const input = await screen.findByPlaceholderText("Search");
     userEvent.type(input, "fullmetal");
     const clear_button = await screen.findByRole("button", {
       name: "Clear",
@@ -31,9 +41,14 @@ describe("When the searchbar is not empty", () => {
 
 describe("When you have some text in the searchbar and you click on the clear button", () => {
   test("The searchbar should become empty", async () => {
-    render(<SearchBar initialData={initialData} setData={mockedSetData} />);
+    render(
+      <SearchBar
+        initialAnimeList={initialAnimeList}
+        setAnimeList={mockedSetAnimeList}
+      />
+    );
 
-    const input = await screen.findByPlaceholderText("Search...");
+    const input = await screen.findByPlaceholderText("Search");
     userEvent.type(input, "fullmetal");
     const clear_button = await screen.findByRole("button", {
       name: "Clear",
