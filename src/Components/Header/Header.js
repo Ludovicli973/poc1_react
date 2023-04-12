dd9d67635efa8f3b1d4a9ee12b5858e2c05480ac
IssamLynx <hm_bakha@esi.dz>
2023-04-12 12:23:19 +0200

    Added workflow

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
       <Link to="/">
         <img src={Logo} alt="Zenika Logo" class="h-full aspect-auto w-[90px]" />
       </Link>
-      <a>Valeurs</a>
-      <a>Offres</a>
-      <a>Green IT</a>
-      <a>Sécurité</a>
+      <a href="/">Valeurs</a>
+      <a href="/">Offres</a>
+      <a href="/">Green IT</a>
+      <a href="/">Sécurité</a>
     </div>
   );
 };
